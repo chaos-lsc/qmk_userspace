30f189a191d25dc99474c8a74b85080e4e4dea93
ChaosLiu <1150180619@qq.com>
2024-10-02 01:06:56 +0800

    change some configs

--- keyboards/bastardkb/charybdis/4x6/keymaps/vendor/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/keymaps/vendor/rules.mk
@@ -1,3 +1,10 @@
+#  enable configuraiton with VIA
 VIA_ENABLE = yes
+
+# trackball & mousekeys
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = pmw3360
+
+CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE =yes
+
+SERIAL_DRIVER = vendor
